5464797814a890b6ebfaddf032cd74616d9ab6d4
Bowen Xu <jimmyxbw@gmail.com>
2019-03-28 15:27:37 -0400

    log4j modify

--- src/main/java/bowen/MyTask.java
+++ src/main/java/bowen/MyTask.java
@@ -15,7 +15,7 @@ public class MyTask {
 	private static final String URL2 = "https://xvevmapi.vmail.comcast.net/";
 	private static final String URL3 = "https://web-po-bvip.vmail.comcast.net/";
 	
-	@Scheduled(fixedRate = 1000*10)
+	@Scheduled(fixedRate = 1000*60*60*24*7) //trigger every week
     public void work() {
 		List<String> recipients = Arrays.asList(TO_EMAIL1, TO_EMAIL2);
 		List<String> urls = Arrays.asList(URL1, URL2, URL3);
